7aea3f527553630484990fa8079158a11e2d28e6
Denys Smirnov <dennwc@pm.me>
2021-10-02 19:18:16 +0300

    Add an option to disable map change.

--- cmd/opennox-control/main.go
+++ cmd/opennox-control/main.go
@@ -9,15 +9,19 @@ import (
 )
 
 var (
-	apiURL    = flag.String("api", "http://127.0.0.1:18580", "base URL for the API endpoint")
-	apiToken  = flag.String("token", "", "API token for controlling the server")
-	allowCmds = flag.Bool("cmds", true, "allow sending commands to the server")
-	bindHost  = flag.String("host", "127.0.0.1:8080", "host to listen on (:8080 to allow external access)")
+	apiURL         = flag.String("api", "http://127.0.0.1:18580", "base URL for the API endpoint")
+	apiToken       = flag.String("token", "", "API token for controlling the server")
+	allowCmds      = flag.Bool("cmds", true, "allow sending commands to the server")
+	allowMapChange = flag.Bool("maps", true, "allow changing maps on the server")
+	bindHost       = flag.String("host", "127.0.0.1:8080", "host to listen on (:8080 to allow external access)")
 )
 
 func main() {
 	flag.Parse()
 	game := octl.NewGameHTTP(*apiURL, *apiToken)
-	cp := octl.NewControlPanel(game, *allowCmds)
+	cp := octl.NewControlPanel(game, &octl.Options{
+		AllowMapChange: *allowMapChange,
+		AllowCommands:  *allowCmds,
+	})
 	log.Fatal(http.ListenAndServe(*bindHost, cp))
 }
